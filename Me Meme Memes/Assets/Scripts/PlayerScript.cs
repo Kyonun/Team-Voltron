faf256096e5e7cf92cd91a4cc624af21a703b9ab
ppowell92 <ppowell1@nmsu.edu>
2019-10-06 13:13:21 -0600

    Added basic movement script;

---
+++ Me Meme Memes/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerScript : MonoBehaviour
+{
+    
+    public Vector2 speed = new Vector2(50, 50);
+    public Vector2 jumpHeight;
+   
+    // Store the movement and component
+    private Vector2 movement;
+    private SpriteRenderer playerRenderer;
+    private Rigidbody2D rigidbodyComponent;
+    
+
+    void Awake()
+    {
+        playerRenderer = GetComponent<SpriteRenderer>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Player Movement 
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        float moveHorizontal = speed.x * inputX;
+        float moveVertical = speed.x * inputY;
+
+        movement = new Vector2( moveHorizontal, moveVertical);
+
+        if (playerRenderer != null)
+        {
+            if (movement.x > 0.01f)
+                playerRenderer.flipX = false;
+            else if (movement.x < -0.01f)
+                playerRenderer.flipX = true;
+        }
+
+
+
+
+
+
+            // End Player Movement
+
+            // Player Weapons
+            bool shoot = Input.GetButtonDown("Fire1");
+
+        shoot |= Input.GetButtonDown("Fire2");
+
+        if (shoot)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+
+            if (weapon != null)
+            {
+                weapon.Attack(false);
+                SoundEffectsHelper.Instance.MakePlayerShotSound();
+            } // end if
+        } // end if
+        
+        // End Player Weapons
+
+        // Make sure we are not outside the camera bounds
+
+        var dist = (transform.position - Camera.main.transform.position).z;
+
+        var leftBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 0, dist)
+        ).x;
+
+        var rightBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(1, 0, dist)
+        ).x;
+
+        var topBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 0, dist)
+        ).y;
+
+        var bottomBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 1, dist)
+        ).y;
+
+        transform.position = new Vector3(
+          Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+          Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+          transform.position.z);
+    }
+
+    void FixedUpdate()
+    {
+        if (rigidbodyComponent == null)
+            rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+        rigidbodyComponent.velocity = movement;
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        bool damagePlayer = false;
+
+        // Collision with enemy
+        EnemyScript enemy = collision.gameObject.GetComponent<EnemyScript>();
+        if (enemy != null)
+        {
+            // Kill the enemy
+            HealthScript enemyHealth = enemy.GetComponent<HealthScript>();
+            if (enemyHealth != null) enemyHealth.Damage(enemyHealth.hp);
+
+            damagePlayer = true;
+        }
+
+        // Damage the player
+        if (damagePlayer)
+        {
+            HealthScript playerHealth = this.GetComponent<HealthScript>();
+            if (playerHealth != null) playerHealth.Damage(1);
+        }
+    }
+    /*
+    void OnDestroy()
+    {
+        // Game Over.
+        var gameOver = FindObjectOfType<GameOverScript>();
+        gameOver.ShowButtons();
+    }*/
+}
+
+    
