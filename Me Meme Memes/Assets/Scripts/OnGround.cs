029089664ef51c439e4fec4b3b84e47d559ad735
ppowell92 <ppowell1@nmsu.edu>
2019-10-06 16:06:43 -0600

    Changed movement scripts.

---
+++ Me Meme Memes/Assets/Scripts/OnGround.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGround : MonoBehaviour
+{
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if( collision.collider.tag == "Platform" )
+        {
+            Player.GetComponent<PlayerMovement>().onGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Platform") 
+        {
+            Player.GetComponent<PlayerMovement>().onGround = false;
+        }
+    }
+}
