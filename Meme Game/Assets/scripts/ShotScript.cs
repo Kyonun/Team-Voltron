989fcfa3f8439bd594108c05ee33551ff48fc059
camismacho <camismacho@yahoo.com>
2019-10-06 21:03:33 -0600

    added more assets and demo0 build

---
+++ Meme Game/Assets/scripts/ShotScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Projectile behavior
+/// </summary>
+public class ShotScript : MonoBehaviour
+{
+    // 1 - Designer variables
+
+    /// <summary>
+    /// Damage inflicted
+    /// </summary>
+    public int damage = 1;
+
+    /// <summary>
+    /// Projectile damages player or enemies?
+    /// </summary>
+    public bool isEnemyShot = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 2 - Limited time to live to avoid any leak
+        Destroy(gameObject, 20); //20 sec
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
