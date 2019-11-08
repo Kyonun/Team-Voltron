d1e1755b0fb908637bc1c4f5f1dc16c8225f6e92
Joe <camismacho@yahoo.com>
2019-11-07 17:36:28 -0700

    added lightning game

---
+++ Lightning Game/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    public bool ClickToDestroy = false; //object will be destroyed when clicked
+    public bool ObjToKeep = false; //if true, you'll lose if this gets destroyed
+    public bool ObjToEliminate = false; //if true, this object will help you win
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        if(ClickToDestroy == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Lightning Game/Assets/Scripts/ScreenLimit.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenLimit : MonoBehaviour
+{
+
+    public Text EndGameMessage;
+    public int ObjectsToEliminateAmount;
+    public int ObjectsToEliminateCount;
+
+    void Awake()
+    {
+        ObjectsToEliminateCount = 0;
+
+        ObjectsToEliminateAmount = 0;
+        ObjectManager[] AllObjs = FindObjectsOfType(typeof(ObjectManager)) as ObjectManager[];
+        
+        foreach(ObjectManager Obj in AllObjs)
+        {
+            if(Obj.ObjToEliminate == true)
+            {
+                ObjectsToEliminateAmount += 1;
+            }
+        }
+    }
+
+
+    
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if(other.GetComponent<ObjectManager>().ObjToKeep == true)
+        {
+            EndGameMessage.gameObject.SetActive(true);
+            EndGameMessage.text = "You Lose!";
+            Time.timeScale = 0;
+        }
+
+        if(other.GetComponent<ObjectManager>().ObjToEliminate == true)
+        {
+            ObjectsToEliminateCount += 1;
+
+            if(ObjectsToEliminateCount == ObjectsToEliminateAmount) 
+            {
+                EndGameMessage.gameObject.SetActive(true);
+                EndGameMessage.text = "YOU WIN!";
+                Time.timeScale = 0;
+            }
+        }
+        
+        Destroy (other.gameObject);
+    }
+}
